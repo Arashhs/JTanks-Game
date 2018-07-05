071bd0518002a344050dae227a44c543885fc159
Arash Hajisafi <hajisafiarash@gmail.com>
2018-07-05 20:28:40 +0430

    Tiles now render more efficiently HUGE fps boost

--- src/gameMap/Block.java
+++ src/gameMap/Block.java
@@ -5,12 +5,12 @@ import java.awt.*;
 public class Block extends Rectangle {
     public int id; //Specify block's type (Dirt , grass , etc)
 
-    public Block(Rectangle r , int id) {
+    public Block(Rectangle r, int id) {
         setBounds(r);
         this.id = id;
     }
 
-    public void render(Graphics g){
-        g.fillRect(x, y, width, height);
+    public void render(Graphics2D g) {
+        g.drawImage(Tile.tileSet_soil, x, y, x + width , y + height , x , y , x + Tile.tileSize , y + Tile.tileSize , null);
     }
 }
